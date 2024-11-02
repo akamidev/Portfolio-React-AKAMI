a8cecb2a2d6511134c09b64931d5733c76e75b57
Mehdi <akamimehdi.dev@gmail.com>
2024-11-02 04:45:11 +0100

    Ajoute un composant ScrollToTop pour gérer le défilement et ajoute une route pour le composant ChatGPT

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-// Importe les dépendances nécessaires
-import React from "react"; // Importe React
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importe les composants pour le routage
-import Home from "./Pages/Home/Home"; // Importe le composant Home
-import Competence from "./Pages/Competence/Competence"; // Importe le composant Competence
-import Portfolio from "./Pages/Portfolio/Portfolio"; // Importe le composant Portfolio
-import Contact from "./Pages/Contact/Contact"; // Importe le composant Contact
-import Footer from "./Partials/Footer/Footer"; // Importe le composant Footer
-import Header from "./Partials/Header/Header"; // Importe le composant Header
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Competence from "./Pages/Competence/Competence";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Contact from "./Pages/Contact/Contact";
+import Footer from "./Partials/Footer/Footer";
+import Header from "./Partials/Header/Header";
+import ChatGPT from "./Pages/ChatGPT/ChatGPT"; // Importer le composant 
 
-// Définit et exporte le composant App
 function App() {
   return (
-    // Utilise le composant Router pour gérer le routage
     <Router>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/Competence" element={<Competence />} />
         <Route path="/Portfolio" element={<Portfolio />} />
         <Route path="/Contact" element={<Contact />} />
+        <Route path="/AssistantIA" element={<ChatGPT />} /> {/* Ajouter la route  */}
         <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
@@ -25,4 +25,20 @@ function App() {
   );
 }
 
-export default App; // Exporte le composant App
+// Composant pour gérer le défilement et ajouter une classe spécifique au body
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    if (pathname === "/AssistantIA") {
+      document.body.classList.add("no-scroll-to-top");
+    } else {
+      document.body.classList.remove("no-scroll-to-top");
+    }
+  }, [pathname]);
+
+  return null;
+}
+
+export default App;
